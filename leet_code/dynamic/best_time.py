fc2348a88a60e2d2774c266e12a15eb46fff36a5
labintsev <andrej.labintsev@yandex.ru>
2023-01-13 13:06:09 +0300

    fix issue

--- leet_code/dynamic/best_time.py
+++ leet_code/dynamic/best_time.py
@@ -1,17 +1,25 @@
-# not correct
 from typing import List
 
 
 def max_profit(prices: List[int]) -> int:
-    min_price = min(prices)
-    ind_min = prices.index(min_price)
-    max_price = max(prices[ind_min:])
-    return max_price - min_price
+    min_price = 4000000
+    profit_max = 0
+    for i, p in enumerate(prices):
+        if p < min_price:
+            min_price = p
+        elif p - min_price > profit_max:
+            profit_max = p - min_price
+
+    return profit_max
 
 
-def mp_dyn(prices: List[int]) -> int:
-    pmin, pmax = prices[:2]
-    prof_max = pmax - pmin
+def mp_broot_force(prices: List[int]) -> int:
+    prof_max = 0
+    for i, pmin in enumerate(prices):
+        for p_ in prices[i:]:
+            prof = p_ - pmin
+            prof_max = max(prof, prof_max)
+
     return max(prof_max, 0)
 
 
@@ -25,5 +33,4 @@ def test(f, prices, profit):
 if __name__ == '__main__':
     test(max_profit, [3, 2, 1],                 0)
     test(max_profit, [1, 2, 3, 1, 5, 7, 1],     6)
-    # todo fix it
-    test(max_profit, [2, 4, 1],                 2)
+    test(max_profit, [12, 18, 1, 3],            6)
