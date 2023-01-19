4f50dfcd8d5cce85f368853dbf052c5e54bea4a6
labintsev <andrej.labintsev@yandex.ru>
2023-01-19 11:54:46 +0300

    done house robber

--- leet_code/dynamic/rob_house.py
+++ leet_code/dynamic/rob_house.py
@@ -1,12 +1,12 @@
 from typing import List
 
 
-def robber(houses: List[int]) -> int:
+def robber_recursive(houses: List[int]) -> int:
     def max_profit(l, r):
         if r < l:
             return 0
         elif r == l:
-            profit = houses[r]
+            return houses[r]
         else:
             profit = max(houses[l:r+1])
         idx = houses.index(profit)
@@ -14,6 +14,24 @@ def robber(houses: List[int]) -> int:
         profit += max_profit(idx+2, r)
         return profit
     return max_profit(0, len(houses)-1)
+
+
+def robber_iterative(houses: List[int]) -> int:
+    can_rob = True
+    profit = last_profit = 0
+
+    for x in houses:
+        if can_rob:
+            last_profit = profit
+            profit += x
+            can_rob = False
+        else:
+            if x + last_profit > profit:
+                profit, last_profit = last_profit + x, profit
+            else:
+                can_rob = True
+
+    return profit
 
 
 def test(f, houses, expected):
@@ -24,9 +42,11 @@ def test(f, houses, expected):
 
 
 if __name__ == '__main__':
-    test(robber, [1], 1)
-    test(robber, [1, 2, 3, 4], 6)
-    test(robber, [10, 2, 3, 4], 14)
-    test(robber, [10, 2, 8, 4], 18)
-    # todo fix it
-    test(robber, [0, 0, 0], 0)
+    f = robber_iterative
+    test(f, [2, 3, 2], 4)
+    test(f, [0, 0, 0], 0)
+    test(f, [1], 1)
+    test(f, [1, 2, 3, 4], 6)
+    test(f, [10, 2, 3, 4], 14)
+    test(f, [10, 2, 8, 4], 18)
+
